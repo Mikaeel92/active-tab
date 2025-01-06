1fd076ba0889fe75c0b67bd7524bf9987b8dd684
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-06 11:32:09 +0330

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
 const App = () => {
-//   const foodData = [{
-//     food: 'Burger',
-//     imageAddress: '/public/burger.jpg'
-// },{
-//     food: 'Fish',
-//     imageAddress: '/public/fish.jpg'
-// },{
-//     food: 'Fruit Salad',
-//     imageAddress: '/public/fruit-salad.jpg'
-// },{
-//     food: 'Pancake',
-//     imageAddress: '/public/pancake.jpg'
-// },{
-//     food: 'Pizza',
-//     imageAddress: '/public/pizza.jpg'
-// }]
+  const foodData = [{
+    food: 'Burger',
+    imageAddress: '/public/burger.jpg'
+},{
+    food: 'Fish',
+    imageAddress: '/public/fish.jpg'
+},{
+    food: 'Fruit Salad',
+    imageAddress: '/public/fruit-salad.jpg'
+},{
+    food: 'Pancake',
+    imageAddress: '/public/pancake.jpg'
+},{
+    food: 'Pizza',
+    imageAddress: '/public/pizza.jpg'
+}]
 
   return (
     <div>App</div>
